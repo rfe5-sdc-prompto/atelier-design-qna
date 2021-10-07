b21ee75321079b706b058ab64bd831b94471628e
Chris Chung <chris.ch.987@gmail.com>
2021-10-07 12:07:29 -0400

    Change host to constant ip

--- db.js
+++ db.js
@@ -4,8 +4,9 @@ const pool = new Pool({
   user: process.env.DB_USER || 'ubuntu',
   password: process.env.DB_PASSWORD || 'ubuntu',
   database: process.env.DB_DATABASE || 'qna',
-  host: process.env.DB_HOST || '18.220.107.64',
+  host: process.env.DB_HOST || '172.31.44.82',
   port: process.env.PORT || 5432,
 });
+
 
 module.exports = pool;
